f0a7dc5702e562770912ae3287772db77385fcb9
Gotsikas <chris.gotsikas@athmew-nm2209.local>
2022-07-17 00:08:29 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Frontend Mentor - Manage landing page
+
+![Design preview for the Manage landing page coding challenge](./design/desktop-preview.jpg)
+This is a solution to the [Manage landing page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/manage-landing-page-SLXqC6P5). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+### Links
+
+- Solution URL: [https://github.com/xphstos/manage-landing-page](https://github.com/xphstos/manage-landing-page)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
---
+++ src/js/app.js
@@ -0,0 +1,59 @@
+import { Splide } from "@splidejs/splide";
+
+const HEADER = document.querySelector("body > .header");
+const TOGGLE = document.querySelector(".toggle");
+const BODY = document.body;
+const HTML = document.documentElement;
+const SPLIDE = document.querySelector(".splide");
+const mediaQueryList = window.matchMedia("(max-width: 820px)");
+
+const slider = new Splide(SPLIDE, {
+  type: "loop",
+  perPage: 3,
+  arrows: false,
+  pagination: false,
+  breakpoints: {
+    1024: {
+      perPage: 2,
+    },
+    768: {
+      perPage: 1,
+      pagination: true,
+    },
+  },
+}).mount();
+
+function getHeaderHeight() {
+  HTML.style.cssText += `--header-height: ${
+    HEADER.getBoundingClientRect().height
+  }px`;
+}
+
+function toggleMenu() {
+  BODY.classList.toggle(
+    "no-scroll",
+    mediaQueryList.matches && !HEADER.classList.contains("header--is-open")
+  );
+  HEADER.classList.toggle(
+    "header--is-open",
+    mediaQueryList.matches && !HEADER.classList.contains("header--is-open")
+  );
+}
+
+function handleScroll() {
+  const { scrollY } = this;
+
+  HEADER.classList.toggle("header--scrolling", scrollY > 0);
+}
+
+function handleMediaQueryList() {
+  HEADER.classList.toggle("header--is-open", false);
+  BODY.classList.toggle("no-scroll", false);
+  getHeaderHeight();
+}
+
+TOGGLE.addEventListener("click", toggleMenu);
+mediaQueryList.addEventListener("change", handleMediaQueryList);
+window.addEventListener("scroll", handleScroll);
+
+getHeaderHeight();
